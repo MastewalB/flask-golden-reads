9deeb91196268af75fd49c4063e3abf24f694150
Mastewal B <se.mastewal.belete@gmail.com>
2021-04-17 07:02:37 +0300

    Login route

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
-
-from flask import Flask, session, render_templates, request, redirect, url_for, jsonify
+import requests
+from flask import Flask, session, render_template, request, redirect, url_for, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,38 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project One: TODO"
+    return render_template('base.html')
+
+def get_google_books_data(isbn):
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={isbn}&key=AIzaSyDTPZR0X5RS-Vb7k00pG9QfmykL-yTE514")
+    value = response.json()
+    #print(value)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    isbn = request.args.get('next')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if password1!=password2:
+            return render_template('signup.html',message="Password did not match.")
+           
+        try:
+            password = password_hash(password1)
+            user = db.execute("insert into users (username,password) values(:username ,:password);",{'username':username,'password':password})
+            db.commit()
+            session['logged_in'] = True
+            session['username'] = username
+            if isbn:
+                return redirect(url_for("book",isbn=isbn))
+            return redirect(url_for("search"))
+        except:
+            return render_template('signup.html',message="Username Exists. Try Another.")
+    return render_template('signup.html',next=isbn)
+
+
+get_google_books_data(9781632168146)
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
