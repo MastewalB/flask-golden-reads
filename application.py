c0732d35465e632c3e9af2a1b42fd1a7e9c2d15f
Mastewal B <se.mastewal.belete@gmail.com>
2021-04-24 05:13:12 +0300

    Review Form, Image Loading, Review Submission

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import requests
 import hashlib
 from flask import Flask, session, render_template, request, redirect, url_for, jsonify, flash
 from flask_session import Session
-from forms import RegistrationForm, LoginForm, SearchForm
+from forms import RegistrationForm, LoginForm, SearchForm, ReviewForm
 from flask_bcrypt import Bcrypt 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -45,23 +45,30 @@ def make_session_permanent():
 
 
 def get_google_books_data(isbn):
-    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={int(isbn)}&key=AIzaSyDTPZR0X5RS-Vb7k00pG9QfmykL-yTE514").json()
+    fallback = "https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+    response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={isbn}&key=AIzaSyDTPZR0X5RS-Vb7k00pG9QfmykL-yTE514").json()
     items = response.get("items")
     rating = 0
+    ratingsCount = 0
     pageNum = 0
     description = ""
+    image_link = fallback
 
     if items:
         try:
             rating = items[0].get("volumeInfo").get("averageRating")
+            ratingsCount = items[0].get("volumeInfo").get("ratingsCount")
             description = items[0].get("volumeInfo").get("description")
             pageNum = items[0].get("volumeInfo").get("pageCount")
+            image_link = items[0].get("volumeInfo").get("imageLinks").get("thumbnail")
         except:
             rating = 0
+            ratingsCount = 0
             pageNum = 0
             description = ""
+            image_link = fallback
     
-    return [pageNum, rating, description]
+    return [pageNum, rating, description, image_link, ratingsCount]
 
 
 @app.route("/signup", methods=['GET', 'POST'])
@@ -112,6 +119,8 @@ def login():
 @app.route("/books", methods=['GET', 'POST'])
 def books():
     form = SearchForm()
+    result_images = []
+    
     if form.validate_on_submit():
         quest = form.search.data
         if quest != None:
@@ -119,18 +128,27 @@ def books():
             result_books = db.execute("SELECT * from books where isbn LIKE ('%"+quest+"%')  or lower(title) LIKE lower('%"+quest+"%') or  lower(author) LIKE lower('%"+quest+"%') order by year desc;").fetchall()
             result_count = len(result_books)
             
+            #image_link = ""
+            for i in range(len(result_books)):
+                image_link = get_google_books_data(result_books[i].isbn)[3]
+                result_images.append(image_link)
+            
+                
             if result_count == 0:
-                return render_template('books.html', form=form, title='Search Results', quest=quest,result_count=result_count,message="404 Not Found")
+                return render_template('books.html', form=form, title='404', quest=quest,result_count=result_count,message="404 Not Found"), 404
 
-            return render_template('books.html', form=form , title='Search Results', quest=quest,result_count=result_count,result_books=result_books)
+            return render_template('books.html', form=form , title='Search Results', quest=quest,result_count=result_count,result_books=result_books, result_images=result_images )
     return render_template('books.html', form=form)
 
 
 @app.route("/book/<isbn>", methods=['GET', 'POST'])
 def book(isbn):
+    form = ReviewForm()
     message = None
     error = False
     is_reviewed = False
+    fallback = "https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+
     if request.method == 'POST' and session['logged_in']:
         my_rating = int(request.form.get('rating'))
         my_review = request.form.get('review')
@@ -168,10 +186,13 @@ def book(isbn):
         except:
             pass
     try:
-        pageNum, rating, description = get_google_books_data(isbn)
+        pageNum, rating, description, image_link, ratingsCount = get_google_books_data(isbn)
     except:
         error = True
-        pageNum, rating, description = 0, 0, 0    
+        pageNum, rating, description, image_link, ratingsCount = 0, 0, "", fallback, 0
+    
+    return render_template('book.html',obj_book=res, form=form, pageNum=pageNum, description=description, image_link=image_link, rating=rating,ratingsCount=ratingsCount, reviews=reviews,message=message,is_reviewed=is_reviewed,error=error)
+   
 
 
 
