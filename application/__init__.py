ee7feb3d4307f9627514eac5a0454361e3364fbe
Mastewal B <se.mastewal.belete@gmail.com>
2021-05-02 14:18:43 +0300

    Flask Login Manager

--- application/__init__.py
+++ application/__init__.py
@@ -5,11 +5,13 @@ from flask import Flask
 from flask_session import Session
 from flask_bcrypt import Bcrypt 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '387b28b25302f3d780ed53706cdeed2e'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -23,8 +25,10 @@ if not os.getenv("DATABASE_URL"):
 #engine = create_engine(os.getenv("DATABASE_URL"))
 #db = scoped_session(sessionmaker(bind=engine))
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.loggin_message_category = 'info'
 
 
 from application import routes
